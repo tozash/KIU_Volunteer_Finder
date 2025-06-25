04c4ef13d7f76722f66a227dbfdec5d0dd269a4b
Alexander Oniani <alexanderoniani6@gmail.com>
2025-06-25 20:04:48 +0400

    add initial data models

---
+++ backend/src/models/event.ts
@@ -0,0 +1,10 @@
+export interface Event {
+  event_id: string;
+  creator_user_id: string;
+  image_url: string;
+  start_date: string; 
+  end_date: string;
+  // location: Location;
+  description: string;
+  volunteer_form: string[];
+}
